4b5470908749077fccf39d643742b80bbb3883bb
Neal Patel <nealp9084@gmail.com>
2014-04-27 00:36:26 -0400

    Added client installation script

---
+++ install-client.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt-get install python2.7 2&> /dev/null
+sudo apt-get install python-pip 2&> /dev/null
+echo -e "\e[1;37m\e[1;42mInstalled python 2.7 and pip\!\e[0m"
+
+sudo apt-get install sqlite3 2&> /dev/null
+echo -e "\e[1;37m\e[1;42mInstalled sqlite3\!\e[0m"
+
+echo -e "\e[1;36m\e[1;44mNow installing python packages with pip...\e[0m"
+sudo pip install sqlite3
+sudo pip install requests
+sudo pip install watchdog
+sudo pip install python-dateutil
+echo -e "\e[1;37m\e[1;42mInstalled all python packages\!\e[0m"
+
+echo -e "\e[1;37m\e[1;42mInstallation complete. After you create an account, you can start using the client.\e[0m"
